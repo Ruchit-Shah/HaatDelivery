d35bfa7612421ec60a86eb46672f116c01f85e5a
Ruchit <ruchit.hungryminds@gmail.com>
2025-08-27 10:07:18 +0530

    First commit

---
+++ HaatDelivery/HaatDelivery/HaatDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaatDeliveryApp.swift
+//  HaatDelivery
+//
+//  Created by Ruchit on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HaatDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
